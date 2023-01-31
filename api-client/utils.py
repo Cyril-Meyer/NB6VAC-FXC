d4757eda5440e1ef870fa6804123cd3645c42ef2
Cyril-Meyer <contact@cyrilmeyer.eu>
2023-02-01 00:03:22 +0100

    add utility function for confirmation

--- api-client/utils.py
+++ api-client/utils.py
@@ -1,3 +1,4 @@
+import sys
 from collections import defaultdict
 
 
@@ -23,3 +24,26 @@ def etree_to_dict(t):
         else:
             d[t.tag] = text
     return d
+
+
+# source : https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
+def query_yes_no(question, default="yes"):
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
